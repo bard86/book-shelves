25b2380d5be2de04b1afc0baf0efd64bf46a6c01
speppa201 <samuel_peppard@comcast.com>
2018-04-14 21:04:53 -0400

    add new package

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
+import BookShelves from './BookShelves'
 import Search from './Search'
-import BookShelves from './BookShelves';
+import NotFound from './NotFound'
 
 class BooksApp extends Component {
+    // we have two properties in state, books for the initial api call, and searchResults for filtered results
     state = {
-        books: [],
-        searchResults: []
+        books: []
     }
 
     componentDidMount() {
@@ -25,45 +26,28 @@ class BooksApp extends Component {
         })
     }
 
-    searchBooks = (query) => {
-        BooksAPI.search(query).then((books) => {
-
-            if (books) {
-                if (!books.error) {
-                    this.setState({
-                        searchResults: books
-                    })
-                } else {
-                    this.setState({
-                        searchResults: []
-                    })
-                }
-            } else {
-                this.setState({
-                    searchResults: []
-                })
-            }
-        })
-    };
-
     render() {
         const { books } = this.state
         return (
+        // we have a main route and a search route
         <div className="app">
-            <Route exact path="/" render={() => (
-                <BookShelves
-                    books={books}
-                    updateBook={this.updateBook}
-                />
-            )}/>
+            <Switch>
+                <Route exact path="/" render={() => (
+                    <BookShelves
+                        books={books}
+                        updateBook={this.updateBook}
+                    />
+                )}/>
 
-            <Route path="/search" render={() => (
-                <Search
-                    searchResults={this.state.searchResults}
-                    searchBooks={this.searchBooks} 
-                    updateBook={this.updateBook}
-                />
-            )}/>
+                <Route path="/search" render={() => (
+                    <Search
+                        searchBooks={this.searchBooks} 
+                        updateBook={this.updateBook}
+                        books={books}
+                    />
+                )}/>
+                <Route component={NotFound}/>
+            </Switch>
         </div>
         )
     }
--- src/Search.js
+++ src/Search.js
@@ -1,38 +1,67 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Book from './Book'
+import * as BooksAPI from './BooksAPI'
 import sortBy from 'sort-by'
 import PropTypes from 'prop-types'
+import { DebounceInput } from 'react-debounce-input';
 
 class Search extends Component {
     state = {
         query: '',
+        searchedBooks: []
     }
 
+    searchBooks = (query) => {
+        BooksAPI.search(query).then((searchResults) => {
+
+            if (searchResults) {
+                // this is to make sure what is already on a shelf has a proper default value when searched
+                // the match is made via the title property and will return what is on a shelf or the search result
+                const getResults = searchResults.map((bookResult) => {
+                    const shelfMatch = this.props.books.find((book) => book.title === bookResult.title)
+                    return shelfMatch || bookResult
+                })
+
+                // if there is no error, set the searchResults state to what is returned in the getResults matching function
+                if (!searchResults.error) {
+                    this.setState({
+                        searchedBooks: searchResults ? getResults : []
+                    })
+                }
+            } else {
+                // this handles if the search does not return anything
+                this.setState({ searchedBooks: [] })
+            }
+        })
+    };
+
     updateQuery = (query) => {
-        this.props.searchBooks(query)
+        // our argument, query will in this case be the value of the input element, which in turn will be set the state for the query property
+        this.searchBooks(query)
         this.setState({ query })
     }
-    
+
     render() {
-        const { searchResults, updateBook } = this.props
+        const { updateBook } = this.props
+        const { queryState, searchedBooks } = this.state
         return (
             <div className="search-books">
                 <div className="search-books-bar">
                     <Link to="/" className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
-                        <input 
-                            type="text" 
+                        <DebounceInput
+                            minLength={1}
+                            debounceTimeout={500}
                             placeholder="Search by title or author"
-                            value={this.state.query}
+                            value={queryState}
                             onChange={(event) => this.updateQuery(event.target.value)}
                         />
-
                     </div>
                 </div>
                 <div className="search-books-results">
                     <ol className="books-grid">
-                        {searchResults.sort(sortBy('title')).map((book) => (
+                        {queryState === '' || searchedBooks.sort(sortBy('title')).map((book) => (
                             <li key={book.id}>
                                 <Book
                                     book={book}
@@ -48,7 +77,7 @@ class Search extends Component {
 }
 
 Search.propTypes = {
-    searchResults: PropTypes.array.isRequired,
+    searchedBooks: PropTypes.array.isRequired,
     updateBook: PropTypes.func.isRequired
 }
 
